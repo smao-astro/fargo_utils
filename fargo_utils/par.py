1dec32fbb483c876f489424d09d01edfefe90293
Shunyuan Mao <54871380+smao-astro@users.noreply.github.com>
2021-08-15 17:56:24 +0800

    compile

--- fargo_utils/par.py
+++ fargo_utils/par.py
@@ -1,12 +1,15 @@
 import argparse
 import pathlib
-
+import numpy as np
 
 def args_to_lines(args: argparse.Namespace):
     """Only process args that not None."""
     arg_list = []
     for k, v in args.__dict__.items():
         if v is not None:
+            if isinstance(v, float):
+                # scientific notation like 1e-5 (rather than 1.0e-5) won't work with fargo3d
+                v = np.format_float_scientific(v, trim='0')
             arg_list.append(k + "\t" + str(v) + "\n")
     return arg_list
 
