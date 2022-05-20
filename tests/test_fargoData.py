6bb4154ff568dd2932318e18799af4d39e7c5cd9
Shunyuan Mao <54871380+smao-astro@users.noreply.github.com>
2022-05-20 11:32:40 +0800

    reformat test_fargoData.py

--- tests/test_fargoData.py
+++ tests/test_fargoData.py
@@ -1,21 +1,32 @@
 import numpy as np
+import pytest
 
-from .fargoData import TimeSeqData
+from fargo_data_process.fargoData import TimeSeqData, Coor
 
 
-def test_flt_x_ymin_t_value():
-    tsdata = TimeSeqData("/Users/kyika/project/pinn/fargo_utils/tmp/fargo3d/outputs", "dens")
+@pytest.fixture
+def output_dir():
+    return "/Users/kyika/project/pinn/fargo_utils/tmp/fargo3d/outputs"
+
+
+def test_coor_get_x_edge(output_dir):
+    coor = Coor(output_dir)
+    assert coor.x_edge.shape == (int(coor._setup["NX"]) + 1,)
+
+
+def test_flt_x_ymin_t_value(output_dir):
+    tsdata = TimeSeqData(output_dir, "dens")
     assert (
         tsdata.flt_x_ymin_t_value.ndim == 2 and tsdata.flt_x_ymin_t_value.shape[1] == 4
     )
 
 
-def test_flt_rmin_theta_t_value():
-    tsdata = TimeSeqData("/Users/kyika/project/pinn/fargo_utils/tmp/fargo3d/outputs", "dens")
+def test_flt_rmin_theta_t_value(output_dir):
+    tsdata = TimeSeqData(output_dir, "dens")
     assert len(np.unique(tsdata.flt_rmin_theta_t_value[:, 0])) == 1
 
 
-def test_to_cartesian():
-    tsdata = TimeSeqData("/Users/kyika/project/pinn/fargo_utils/tmp/fargo3d/outputs", "dens")
+def test_to_cartesian(output_dir):
+    tsdata = TimeSeqData(output_dir, "dens")
     value = tsdata.frames[0].to_cartesian(1000)
     assert 1
