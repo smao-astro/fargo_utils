f7a6f8654c12af9bbe8fe1da4e7418b079d30230
Shunyuan Mao <54871380+smao-astro@users.noreply.github.com>
2022-05-20 10:51:08 +0800

    move tests to tests/

--- tests/test_fargoData.py
+++ tests/test_fargoData.py
@@ -4,18 +4,18 @@ from .fargoData import TimeSeqData
 
 
 def test_flt_x_ymin_t_value():
-    tsdata = TimeSeqData("/Users/kyika/project/pinn/disk2D/job/ring/outputs", "dens")
+    tsdata = TimeSeqData("/Users/kyika/project/pinn/fargo_utils/tmp/fargo3d/outputs", "dens")
     assert (
         tsdata.flt_x_ymin_t_value.ndim == 2 and tsdata.flt_x_ymin_t_value.shape[1] == 4
     )
 
 
 def test_flt_rmin_theta_t_value():
-    tsdata = TimeSeqData("/Users/kyika/project/pinn/disk2D/job/ring/outputs", "dens")
+    tsdata = TimeSeqData("/Users/kyika/project/pinn/fargo_utils/tmp/fargo3d/outputs", "dens")
     assert len(np.unique(tsdata.flt_rmin_theta_t_value[:, 0])) == 1
 
 
 def test_to_cartesian():
-    tsdata = TimeSeqData("/Users/kyika/project/pinn/disk2D/job/ring/outputs", "dens")
+    tsdata = TimeSeqData("/Users/kyika/project/pinn/fargo_utils/tmp/fargo3d/outputs", "dens")
     value = tsdata.frames[0].to_cartesian(1000)
     assert 1
