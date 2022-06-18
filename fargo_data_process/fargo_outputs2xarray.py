a2d422fdb121c78765e6f3e99df68af618f7b91c
Shunyuan Mao <54871380+smao-astro@users.noreply.github.com>
2022-06-18 09:11:42 +0800

    fargo_outputs2xarray.py save files to fargo:run

--- fargo_data_process/fargo_outputs2xarray.py
+++ fargo_data_process/fargo_outputs2xarray.py
@@ -1,11 +1,27 @@
-import os
+import pathlib
 
 from .config import get_config
 from .fargoData import TimeSeqData
 
 
+def resolve_save_dir(output_dir, file_list, verbose=True):
+    save_dir = pathlib.Path(output_dir)
+    # resolve soft links
+    for file in file_list:
+        if (save_dir / file).exists():
+            save_dir = (save_dir / file).resolve().parent
+            break
+    else:
+        raise FileNotFoundError(f"Can not find {file_list} in {save_dir}")
+
+    if verbose:
+        print(f"save_dir={save_dir}")
+    return save_dir
+
+
 def main(output_dir):
     """convert fargo outputs to npz file with (N, 4) shape."""
+    save_dir = resolve_save_dir(output_dir, ["variables.par"])
 
     dens = TimeSeqData(output_dir, phys_var_type="dens").xarray
     print(f"dens shape {dens.shape}")
@@ -13,9 +29,9 @@ def main(output_dir):
     print(f"vx shape {vx.shape}")
     vy = TimeSeqData(output_dir, phys_var_type="vy").xarray
     print(f"vy shape {vy.shape}")
-    dens.to_netcdf(os.path.join(output_dir, "test_dens.nc"))
-    vx.to_netcdf(os.path.join(output_dir, "test_vx.nc"))
-    vy.to_netcdf(os.path.join(output_dir, "test_vy.nc"))
+    dens.to_netcdf(save_dir / "test_dens.nc")
+    vx.to_netcdf(save_dir / "test_vx.nc")
+    vy.to_netcdf(save_dir / "test_vy.nc")
 
 
 if __name__ == "__main__":
