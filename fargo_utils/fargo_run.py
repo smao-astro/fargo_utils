smao-astro/fargo_utils

commit d08933c8a20eb043766b95f9435534e528102a9d

fargo:setup for make process

fargo_utils/fargo_run.py
@@ -1,0 +1,152 @@
+"""
+1. Write to setup file
+2. run
+"""
+import math
+import os
+import pathlib
+import subprocess
+
+from . import config
+from . import par
+
+
+def get_parser():
+    """
+    References:
+        std/stdpar.par
+    Returns:
+
+    """
+    parser = config.MyArgumentParser("run", fromfile_prefix_chars="%")
+
+    parser.add_argument("--Nx", type=int)
+    parser.add_argument("--Ny", type=int)
+    parser.add_argument("--Nz", type=int)
+    parser.add_argument("--Xmin", type=float, default=-math.pi)
+    parser.add_argument("--Xmax", type=float, default=math.pi)
+    parser.add_argument("--Ymin", type=float)
+    parser.add_argument("--Ymax", type=float)
+    parser.add_argument("--Zmin", type=float)
+    parser.add_argument("--Zmax", type=float)
+    parser.add_argument("--Oorta", type=float)
+    parser.add_argument("--SigmaSlope", type=float)
+    parser.add_argument("--Eccentricity", type=float)
+    parser.add_argument("--Inclination")
+    parser.add_argument("--ThicknessSmoothing", type=float)
+    parser.add_argument("--RocheSmoothing", type=float)
+    parser.add_argument("--AspectRatio", type=float)
+    parser.add_argument("--Sigma0", type=float)
+    parser.add_argument("--FlaringIndex", type=float)
+    parser.add_argument("--Nu", type=float)
+    parser.add_argument("--Cs", type=float)
+    parser.add_argument("--Frame", choices=["F", "C", "G"])
+    parser.add_argument("--OmegaFrame", type=float)
+    parser.add_argument("--IndirectTerm", choices=["yes", "no"])
+    parser.add_argument("--ExcludeHill", choices=["yes", "no"])
+    parser.add_argument("--MassTaper", type=float)
+    parser.add_argument("--Noise", type=float)
+    parser.add_argument("--VerticalDamping", type=float)
+    parser.add_argument("--Spacing")
+    parser.add_argument("--Coordinates")
+    parser.add_argument("--Gamma", type=float)
+    parser.add_argument("--Alpha", type=float)
+    parser.add_argument("--PlanetConfig", type=str)
+    parser.add_argument("--PeriodicZ", choices=["yes", "no"])
+    parser.add_argument("--PeriodicY", choices=["yes", "no"])
+
+    parser.add_argument("--OhmicDiffusionCoeff", type=float)
+    parser.add_argument("--HallEffectCoeff", type=float)
+    parser.add_argument("--AmbipolarDiffusionCoeff", type=float)
+
+    parser.add_argument("--Resonance", type=float)
+
+    parser.add_argument("--DampingZone", type=float)
+    parser.add_argument("--TauDamp", type=float)
+
+    parser.add_argument("--KillingBCColatitude", type=float)
+
+    parser.add_argument("--Beta", type=float)
+    parser.add_argument("--CFL", type=float)
+
+    parser.add_argument("--Vtk", choices=["yes", "no"])
+    parser.add_argument("--RealType", choices=["Standard", "float", "double"])
+
+    parser.add_argument("--PlanetMass", type=float)
+    parser.add_argument("--Field")
+    parser.add_argument("--Cmap")
+
+    parser.add_argument("--PlotLog", choices=["yes", "no"])
+    parser.add_argument("--Colorbar", choices=["yes", "no"])
+    parser.add_argument("--Autocolor", choices=["yes", "no"])
+    parser.add_argument("--Aspect")
+    parser.add_argument("--vmin", type=float)
+    parser.add_argument("--vmax", type=float)
+    parser.add_argument("--PlotLine")
+
+    parser.add_argument("--OrbitalRadius", type=float)
+    parser.add_argument("--ReleaseDate", type=float)
+    parser.add_argument("--ReleaseRadius", type=float)
+    parser.add_argument("--SemiMajorAxis", type=float)
+
+    parser.add_argument("--FuncArchFile")
+
+    parser.add_argument("--Nsnap", type=int)
+    parser.add_argument("--WriteDensity", choices=["yes", "no"])
+    parser.add_argument("--WriteEnergy", choices=["yes", "no"])
+    parser.add_argument("--WriteBx", choices=["yes", "no"])
+    parser.add_argument("--WriteBy", choices=["yes", "no"])
+    parser.add_argument("--WriteBz", choices=["yes", "no"])
+    parser.add_argument("--WriteVx", choices=["yes", "no"])
+    parser.add_argument("--WriteVy", choices=["yes", "no"])
+    parser.add_argument("--WriteVz", choices=["yes", "no"])
+    parser.add_argument("--WriteDivergence", choices=["yes", "no"])
+    parser.add_argument("--WriteEnergyRad", choices=["yes", "no"])
+    parser.add_argument("--WriteTau", choices=["yes", "no"])
+
+    # not in std/stdpar.par
+    parser.add_argument("--DT", type=float, default=math.pi / 100.0)
+    parser.add_argument("--Ninterm", type=int)
+    parser.add_argument("--Ntot", type=int)
+    parser.add_argument("--OutputDir", type=str)
+
+    # ring specific args
+    parser.add_argument("--RingCenter", type=float)
+    parser.add_argument("--RingWidth", type=float)
+
+    return parser
+
+
+def write_par_file(setup_dir: pathlib.Path, arg_groups):
+    # # par file
+    par_file = setup_dir / (arg_groups["optional arguments"].job_name + ".par")
+    par.write_args(par_file, args={**vars(arg_groups["par"]), **vars(arg_groups["ic"])})
+
+
+def run(arg_groups):
+    # run
+    os.chdir("fargo3d")
+    subprocess.run(
+        [
+            "./fargo3d",
+            f"{arg_groups['optional arguments'].job_name}.par",
+        ]
+    )
+
+
+if __name__ == "__main__":
+    args = get_parser().parse_args()
+    fargo_dir = pathlib.Path("fargo3d")
+    arg_groups = config.load_arg_groups(fargo_dir / "arg_groups.yml")
+
+    # append values to arg_groups
+    arg_groups["par"] = args
+    # delete old arg_groups.yml
+    pathlib.Path.unlink(fargo_dir / "arg_groups.yml")
+    config.save_arg_groups(
+        arg_groups=arg_groups, file_path=fargo_dir / "arg_groups.yml"
+    )
+
+    write_par_file(setup_dir=fargo_dir, arg_groups=arg_groups)
+
+    run(arg_groups)

fargo_utils/fargo_setups.py
@@ -1,21 +1,22 @@
 import os
 import pathlib
-import subprocess
 import pickle
 import shutil
+import subprocess
+import importlib.resources
 
+from . import setup_base
 from . import boundary
 from . import config
 from . import ic
 from . import opt
-from . import par
 
 
 def create_setups(arg_groups: dict):
     # set setups dir
     p = (
         pathlib.Path(arg_groups["optional arguments"].setups_dir)
-        / arg_groups["par"].Setup
+        / arg_groups["optional arguments"].Setup
     )
     # check exists
     if p.exists():
@@ -25,11 +26,11 @@ def create_setups(arg_groups: dict):
 
     # bound file
     bound_args = boundary.cfg_to_nested_dict(arg_groups["bc"])
-    bound_file = p / (arg_groups["par"].Setup + ".bound")
+    bound_file = p / (arg_groups["optional arguments"].Setup + ".bound")
     boundary.write_boundlines(bound_args, bound_file)
 
     # copy bound to bound.0
-    shutil.copy(bound_file, p / (arg_groups["par"].Setup + ".bound.0"))
+    shutil.copy(bound_file, p / (arg_groups["optional arguments"].Setup + ".bound.0"))
 
     # ic file (move matched file from setup_base)
     ic_file = ic.get_condinit_file(
@@ -40,15 +41,15 @@ def create_setups(arg_groups: dict):
     shutil.copy(ic_file, p / "condinit.c")
 
     # opt file
-    opt_file = p / (arg_groups["par"].Setup + ".opt")
+    opt_file = p / (arg_groups["optional arguments"].Setup + ".opt")
     # with importlib.resources.path(setup_base.opt, "base.opt") as base_opt:
     #     shutil.copy(base_opt, opt_file)
     # opt.update_opt_file(opt_file, arg_groups["opt"])
     opt.write_opt_file(opt_file, arg_groups["opt"])
 
-    # par file
-    par_file = p / (arg_groups["optional arguments"].job_name + ".par")
-    par.write_args(par_file, args={**vars(arg_groups["par"]), **vars(arg_groups["ic"])})
+    # write default par file
+    with importlib.resources.path(setup_base, "setup.par") as par_file:
+        shutil.copyfile(par_file, p / f"{arg_groups['optional arguments'].Setup}.par")
 
     # save arg_groups
     with open(p / "arg_groups.pkl", "wb") as f:
@@ -60,14 +61,14 @@ def create_setups(arg_groups: dict):
 if __name__ == "__main__":
     arg_groups = config.get_arg_groups()
     print(arg_groups)
-    p = create_setups(arg_groups)
-    config.save_arg_groups(arg_groups, p / "arg_groups.yml")
+    setup_dir = create_setups(arg_groups)
+    config.save_arg_groups(arg_groups, "fargo3d/arg_groups.yml")
     # cd fargo3d
     os.chdir("fargo3d")
     # make
     make_command = [
         "make",
-        f"SETUP={arg_groups['par'].Setup}",
+        f"SETUP={arg_groups['optional arguments'].Setup}",
         f"BIGMEM={arg_groups['make'].BIGMEM}",
         f"RESCALE={arg_groups['make'].RESCALE}",
         f"PROFILING={arg_groups['make'].PROFILING}",
@@ -83,10 +84,5 @@ if __name__ == "__main__":
     ]
     print(make_command)
     subprocess.run(make_command)
-    # run
-    subprocess.run(
-        [
-            "./fargo3d",
-            f"setups/{arg_groups['par'].Setup}/{arg_groups['optional arguments'].job_name}.par",
-        ]
-    )
+    # make clean
+    subprocess.run(["make", "clean"])

MANIFEST.in
@@ -1,1 +1,1 @@
-recursive-include fargo_utils *.c *.opt
+recursive-include fargo_utils *.c *.opt *.par

fargo_data_process/fargo_collect_xarrays.py
@@ -107,7 +107,7 @@ def main(runs_dir, yaml_file, save_dir, collecting_mode):
         yaml.safe_dump(fargo_setups, f)
     # save a arg_groups.yml
     run0 = fargo_runs["runs"][0]
-    arg_groups_file = f"{run0}*/fargo3d/setups/*/arg_groups.yml"
+    arg_groups_file = f"{run0}*/fargo3d/arg_groups.yml"
     arg_groups_file = list(runs_dir.glob(arg_groups_file))[0]
     shutil.copy(arg_groups_file, save_dir)
 

fargo_make.yml
@@ -1,0 +1,31 @@
+- setups_dir: fargo3d/setups
+  job_name: fargo2
+  
+  DensityInitial: POWERLAW2DDENS
+  VxInitial: STATICPOWERLAW2DVAZIM
+  VyInitial: STATICVY
+  
+  DensityYmin: KEPLERIAN2DDENS
+  DensityYmax: KEPLERIAN2DDENS
+  VxYmin: KEPLERIAN2DVAZIM
+  VxYmax: KEPLERIAN2DVAZIM
+  VyYmin: ANTISYMMETRIC
+  VyYmax: ANTISYMMETRIC
+
+  # opt
+  NFLUIDS: 1
+  STOCKHOLM: yes
+  X: yes
+  Y: yes
+  ISOTHERMAL: yes
+  CYLINDRICAL: yes
+  LEGACY: yes
+  POTENTIAL: yes
+  VISCOSITY: yes
+  BLOCK_X: 16
+  BLOCK_Y: 16
+  BLOCK_Z: 1
+  MONITOR_SCALAR: 'MASS | MOM_X | TORQ'
+  MONITOR_Y_RAW: 'TORQ'
+
+  Setup: fargo2

fargo_run.yml
@@ -1,35 +1,4 @@
-- setups_dir: fargo3d/setups
-  job_name: fargo2
-  
-  DensityInitial: POWERLAW2DDENS
-  VxInitial: STATICPOWERLAW2DVAZIM
-  VyInitial: STATICVY
-  
-  DensityYmin: KEPLERIAN2DDENS
-  DensityYmax: KEPLERIAN2DDENS
-  VxYmin: KEPLERIAN2DVAZIM
-  VxYmax: KEPLERIAN2DVAZIM
-  VyYmin: ANTISYMMETRIC
-  VyYmax: ANTISYMMETRIC
-
-  # opt
-  NFLUIDS: 1
-  STOCKHOLM: yes
-  X: yes
-  Y: yes
-  ISOTHERMAL: yes
-  CYLINDRICAL: yes
-  LEGACY: yes
-  POTENTIAL: yes
-  VISCOSITY: yes
-  BLOCK_X: 16
-  BLOCK_Y: 16
-  BLOCK_Z: 1
-  MONITOR_SCALAR: 'MASS | MOM_X | TORQ'
-  MONITOR_Y_RAW: 'TORQ'
-
-  Setup: fargo2
-  
+- FuncArchFile: func_arch.cfg
   ### Disk parameters
   
   AspectRatio: 0.05

fargo_utils/config.py
@@ -29,6 +29,9 @@ def get_parser():
     parser.add_argument(
         "--setups_dir", type=str, required=True, help="the path of setups/"
     )
+    parser.add_argument(
+        "--Setup", required=True, help="The name of the whole setup directory."
+    )
     parser.add_argument("--job_name", required=True, help="The name of par file.")
 
     ic_group = parser.add_argument_group("ic")
@@ -103,105 +106,6 @@ def get_parser():
     opt_group.add_argument("--MONITOR_Z_RAW", type=str, default="")
     opt_group.add_argument("--MONITOR_SCALAR", type=str, default="")
 
-    # par (reference: std/stdpar.par)
-    par_group = parser.add_argument_group("par")
-    par_group.add_argument("--Nx", type=int)
-    par_group.add_argument("--Ny", type=int)
-    par_group.add_argument("--Nz", type=int)
-    par_group.add_argument("--Xmin", type=float, default=-math.pi)
-    par_group.add_argument("--Xmax", type=float, default=math.pi)
-    par_group.add_argument("--Ymin", type=float)
-    par_group.add_argument("--Ymax", type=float)
-    par_group.add_argument("--Zmin", type=float)
-    par_group.add_argument("--Zmax", type=float)
-    par_group.add_argument("--Oorta", type=float)
-    par_group.add_argument("--SigmaSlope", type=float)
-    par_group.add_argument("--Eccentricity", type=float)
-    par_group.add_argument("--Inclination")
-    par_group.add_argument("--ThicknessSmoothing", type=float)
-    par_group.add_argument("--RocheSmoothing", type=float)
-    par_group.add_argument("--AspectRatio", type=float)
-    par_group.add_argument("--Sigma0", type=float)
-    par_group.add_argument("--FlaringIndex", type=float)
-    par_group.add_argument("--Nu", type=float)
-    par_group.add_argument("--Cs", type=float)
-    par_group.add_argument("--Frame", choices=["F", "C", "G"])
-    par_group.add_argument("--OmegaFrame", type=float)
-    par_group.add_argument("--IndirectTerm", choices=["yes", "no"])
-    par_group.add_argument("--ExcludeHill", choices=["yes", "no"])
-    par_group.add_argument("--MassTaper", type=float)
-    par_group.add_argument("--Noise", type=float)
-    par_group.add_argument("--VerticalDamping", type=float)
-    par_group.add_argument("--Spacing")
-    par_group.add_argument("--Coordinates")
-    par_group.add_argument("--Gamma", type=float)
-    par_group.add_argument("--Alpha", type=float)
-    par_group.add_argument("--PlanetConfig", type=str)
-    par_group.add_argument("--PeriodicZ", choices=["yes", "no"])
-    par_group.add_argument("--PeriodicY", choices=["yes", "no"])
-
-    par_group.add_argument("--OhmicDiffusionCoeff", type=float)
-    par_group.add_argument("--HallEffectCoeff", type=float)
-    par_group.add_argument("--AmbipolarDiffusionCoeff", type=float)
-
-    par_group.add_argument("--Resonance", type=float)
-
-    par_group.add_argument("--DampingZone", type=float)
-    par_group.add_argument("--TauDamp", type=float)
-
-    par_group.add_argument("--KillingBCColatitude", type=float)
-
-    par_group.add_argument("--Beta", type=float)
-    par_group.add_argument("--CFL", type=float)
-
-    par_group.add_argument("--Vtk", choices=["yes", "no"])
-    par_group.add_argument("--RealType", choices=["Standard", "float", "double"])
-
-    par_group.add_argument("--PlanetMass", type=float)
-    par_group.add_argument("--Field")
-    par_group.add_argument("--Cmap")
-
-    par_group.add_argument("--PlotLog", choices=["yes", "no"])
-    par_group.add_argument("--Colorbar", choices=["yes", "no"])
-    par_group.add_argument("--Autocolor", choices=["yes", "no"])
-    par_group.add_argument("--Aspect")
-    par_group.add_argument("--vmin", type=float)
-    par_group.add_argument("--vmax", type=float)
-    par_group.add_argument("--PlotLine")
-
-    par_group.add_argument("--OrbitalRadius", type=float)
-    par_group.add_argument("--ReleaseDate", type=float)
-    par_group.add_argument("--ReleaseRadius", type=float)
-    par_group.add_argument("--SemiMajorAxis", type=float)
-
-    par_group.add_argument("--FuncArchFile")
-    par_group.add_argument(
-        "--Setup", required=True, help="The name of the whole setup directory."
-    )
-
-    par_group.add_argument("--Nsnap", type=int)
-    par_group.add_argument("--WriteDensity", choices=["yes", "no"])
-    par_group.add_argument("--WriteEnergy", choices=["yes", "no"])
-    par_group.add_argument("--WriteBx", choices=["yes", "no"])
-    par_group.add_argument("--WriteBy", choices=["yes", "no"])
-    par_group.add_argument("--WriteBz", choices=["yes", "no"])
-    par_group.add_argument("--WriteVx", choices=["yes", "no"])
-    par_group.add_argument("--WriteVy", choices=["yes", "no"])
-    par_group.add_argument("--WriteVz", choices=["yes", "no"])
-    par_group.add_argument("--WriteDivergence", choices=["yes", "no"])
-    par_group.add_argument("--WriteEnergyRad", choices=["yes", "no"])
-    par_group.add_argument("--WriteTau", choices=["yes", "no"])
-
-    # not in std/stdpar.par
-    par_group.add_argument("--DT", type=float, default=math.pi / 100.0)
-    par_group.add_argument("--Ninterm", type=int)
-    par_group.add_argument("--Ntot", type=int)
-    par_group.add_argument("--OutputDir", type=str)
-
-    # ring specific args
-    par_group.add_argument("--RingCenter", type=float)
-    par_group.add_argument("--RingWidth", type=float)
-
     # make
     make_group = parser.add_argument_group("make")
     make_group.add_argument("--BIGMEM", type=int, default=1)
@@ -242,7 +146,7 @@ def get_arg_groups(args=None):
     parser = get_parser()
     args = parser.parse_args(args)
 
-    check_dampling_setup(args)
+    # check_dampling_setup(args)
 
     arg_groups = {}
 
@@ -277,5 +181,5 @@ def load_arg_groups(file_path):
     with file_path.open("r") as f:
         arg_groups: dict = yaml.safe_load(f)
 
-    # todo to namespace
+    # to namespace
     return {k: argparse.Namespace(**v) for k, v in arg_groups.items()}

fargo_utils/opt.py
@@ -63,8 +63,11 @@ def write_opt_file(opt_file, opts: dict or argparse.Namespace):
     lines += cuda_blocks
 
     # MONITOR
+    v: str
     for k, v in opts.items():
         if k.startswith("MONITOR") and len(v) > 0:
+            v = v.rstrip("'")
+            v = v.lstrip("'")
             lines.append(f"{k} = {v}")
 
     with open(opt_file, "w") as f:

commit d246030a9311887c02a15631eef17ee89345f180

fixed a bug introduced by d08933c: If paramters are not in the xxx.par file at the make process, they won't in the file outputs/variables.par.

fargo_utils/fargo_run.py
@@ -120,7 +120,10 @@ def get_parser():
 def write_par_file(setup_dir: pathlib.Path, arg_groups):
     # # par file
     par_file = setup_dir / (arg_groups["optional arguments"].job_name + ".par")
-    par.write_args(par_file, args={**vars(arg_groups["par"]), **vars(arg_groups["ic"])})
+    args = {"Setup": arg_groups["optional arguments"].Setup}
+    args.update(vars(arg_groups["par"]))
+    args.update(vars(arg_groups["ic"]))
+    par.write_args(par_file, args=args)
 
 
 def run(arg_groups):

fargo_utils/fargo_setups.py
@@ -4,6 +4,7 @@ import pickle
 import shutil
 import subprocess
 import importlib.resources
+from . import par
 
 from . import setup_base
 from . import boundary
@@ -48,8 +49,16 @@ def create_setups(arg_groups: dict):
     opt.write_opt_file(opt_file, arg_groups["opt"])
 
     # write default par file
-    with importlib.resources.path(setup_base, "setup.par") as par_file:
-        shutil.copyfile(par_file, p / f"{arg_groups['optional arguments'].Setup}.par")
+    par_file = p / f"{arg_groups['optional arguments'].Setup}.par"
+    with importlib.resources.path(setup_base, "setup.par") as base_par_file:
+        shutil.copyfile(base_par_file, par_file)
+    # Setup and ic related parameters are needed in the par file at make time
+    # to make sure these parameters are in the file `outputs/variables.par`.
+    paramters = {"Setup": arg_groups["optional arguments"].Setup}
+    paramters.update(vars(arg_groups["ic"]))
+    lines = par.args_to_lines(paramters)
+    with par_file.open("a") as f:
+        f.writelines(lines)
 
     # save arg_groups
     with open(p / "arg_groups.pkl", "wb") as f:

fargo_utils/par.py
@@ -46,6 +46,6 @@ def get_frame_angular_velocity(frame, omegaframe, planet_distance):
             raise ValueError(
                 f"planet_distance = {planet_distance} is close to zero. Can not set rotating frame."
             )
-        return planet_distance ** -1.5
+        return planet_distance**-1.5
     else:
         raise KeyError